4847cb62e6abcc78947e4792dfa7673acbee3fb5
Hermas Amaewhule <whule@yahoo.com>
2023-11-12 05:34:25 +0000

    Refactored the python file for testing multiple functions

--- milestone_3.py
+++ milestone_3.py
@@ -3,10 +3,22 @@ import random
 word_list = ["orange", "banana", "grapes", "mango", "strawberies"]
 
 
-while True:
+
+def check_guess(guess):
+    guess = guess.lower()
     word = random.choice(word_list)
-    guess = input("Please enter single letter:")
     if guess in word:
-        print(f'Good guess, {guess} is in word')
+        print(f"Good guess, {guess} is in the word")
     else:
         print(f"Sorry, {guess} is not in word")
+
+def ask_for_input():
+    guess = input("Please enter single letter:")
+    while True:
+        if len(guess) == 1 and guess.isalpha == True:
+            return guess
+        else:
+            print("Invalid entry, Please try again!")
+
+    check_guess(guess)
+ask_for_input()
