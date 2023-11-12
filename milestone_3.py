d7fa23c3df07f5e064e2f95c51d50f3be1d5cad0
Hermas Amaewhule <whule@yahoo.com>
2023-11-12 04:55:44 +0000

    Refactored the python file for testing while loop

--- milestone_3.py
+++ milestone_3.py
@@ -1,7 +1,12 @@
+import random
+
+word_list = ["orange", "banana", "grapes", "mango", "strawberies"]
+
 
 while True:
+    word = random.choice(word_list)
     guess = input("Please enter single letter:")
-    if len(guess) == 1 and guess.isalpha() == True:
-        break
+    if guess in word:
+        print(f'Good guess, {guess} is in word')
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in word")
